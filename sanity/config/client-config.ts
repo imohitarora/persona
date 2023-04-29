29a3768a7d8236f513060a775013cf1e4a3acb3d
Mohit Arora <me@aboutmohit.com>
2023-04-29 16:42:23 -0400

    final push

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const clientConfig = {
+  projectId: "j1ok0828",
+  dataset: "production",
+  apiVersion: "2023-04-29",
+  useCdn: true,
+};
+export default clientConfig;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,12 +1,10 @@
 import { createClient, groq } from "next-sanity";
 import { Project } from "../types/Project";
+import clientConfig from "./config/client-config";
+import { Page } from "../types/Page";
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: "j1ok0828",
-    dataset: "production",
-    apiVersion: "2023-04-29",
-  });
+  const client = createClient(clientConfig);
 
   return client.fetch(
     groq`*[_type == "project"]{
@@ -18,5 +16,50 @@ export async function getProjects(): Promise<Project[]> {
             url,
             content
         }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient(clientConfig);
+
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  const client = createClient(clientConfig);
+
+  return client.fetch(
+    groq`*[_type == "page"]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+        }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  const client = createClient(clientConfig);
+
+  return client.fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            title,
+            "slug": slug.current,
+            content
+        }`,
+    { slug }
   );
 }
